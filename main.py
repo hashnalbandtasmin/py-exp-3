8b2c50b91edc030fda850fefe46da072117aa18c
hashnalbandtasmin <nalbandtasmin@eng.rizvi.edu.in>
2026-01-29 22:25:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-# AIM: Write a Python program to calculate the gross salary of an employee.
-# Coder:
-# Date:
+# AIM: Write a Python program to calculate the gross salary of an employ
+# Coder:Tasmin 
+# Date:16/01/2026
 
-# Write your code here
+# write your code here
+
+print("salary calculator:")
+BS= float(input("Enter the Basic salary:"))
+DA = (70/100)*BS
+TA = (30/100)*BS
+HRA = (10/100)*BS
+Gross_salary =BS+ DA + TA + HRA
+
+
+
+print("salary details:")
+print( "Basic salary:",BS)
+print("DA(70%):", (DA))
+print("TA(30%):", (TA))
+print("HRA(10%):", (HRA))
+print("Gross_salary:", (Gross_salary))
